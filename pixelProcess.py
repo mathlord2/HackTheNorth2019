c3ecbe541cc5cc20d1e728b962a634b05b1eebd9
tdude92 <trevorguanhao@gmail.com>
2019-09-14 02:58:29 -0400

    rohanusuck

--- pixelProcess.py
+++ pixelProcess.py
@@ -1,8 +1,18 @@
 #A functional file to process csv pixel information
+import csv
 
 def rgbhex(num):
     hex = "#%02x%02x%02x" % (num, num, num)
     return hex
+
+def getDataSet(fileName):
+    output = []
+    with open(fileName, mode = "r") as csvFile:
+        csvReader = csv.DictReader(csvFile)
+        for row in csvReader:
+            if row["Usage"] == "Training":
+                output.append([int(row["emotion"]), list(map(int, row["pixels"]))])
+    return output
 
 def process(screen, dots):
     count = 0
