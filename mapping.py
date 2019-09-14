dfd8c56b9960778f5de816684bf5086c4f6de14b
tdude92 <trevorguanhao@gmail.com>
2019-09-14 19:32:50 -0400

    ok

---
+++ mapping.py
@@ -0,0 +1,65 @@
+from __future__ import division
+import pixelProcess as pp
+import cv2
+import dlib
+import time
+import sys
+import numpy as np
+
+def shape_to_np(shape, dtype="int"):
+	# initialize the list of (x, y)-coordinates
+	coords = np.zeros((68, 2), dtype=dtype)
+ 
+	# loop over the 68 facial landmarks and convert them
+	# to a 2-tuple of (x, y)-coordinates
+	for i in range(0, 68):
+		coords[i] = (shape.part(i).x, shape.part(i).y)
+ 
+	# return the list of (x, y)-coordinates
+	return coords
+
+def rect_to_bb(rect):
+	# take a bounding predicted by dlib and convert it
+	# to the format (x, y, w, h) as we would normally do
+	# with OpenCV
+	x = rect.left()
+	y = rect.top()
+	w = rect.right() - x
+	h = rect.bottom() - y
+ 
+	# return a tuple of (x, y, w, h)
+	return (x, y, w, h)
+
+def detectFaceDlibHog(detector, frame, inHeight=300, inWidth=0):
+
+    frameDlibHog = frame.copy()
+    frameHeight = frameDlibHog.shape[0]
+    frameWidth = frameDlibHog.shape[1]
+    if not inWidth:
+        inWidth = int((frameWidth / frameHeight)*inHeight)
+
+    scaleHeight = frameHeight / inHeight
+    scaleWidth = frameWidth / inWidth
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    frameDlibHogSmall = cv2.resize(frameDlibHog, (inWidth, inHeight))
+
+    frameDlibHogSmall = cv2.cvtColor(frameDlibHogSmall, cv2.COLOR_BGR2RGB)
+    faceRects = detector(frameDlibHogSmall, 1)
+    bboxes = []
+    for (i, faceRect) in enumerate(faceRects):
+
+        cvRect = [int(faceRect.left()*scaleWidth), int(faceRect.top()*scaleHeight),
+                  int(faceRect.right()*scaleWidth), int(faceRect.bottom()*scaleHeight) ]
+        bboxes.append(cvRect)
+        cv2.rectangle(frameDlibHog, (cvRect[0], cvRect[1]), (cvRect[2], cvRect[3]), (0, 255, 0), int(round(frameHeight/150)), 4)
+
+        shape = predictor(gray, faceRect)
+        shape = shape_to_np(shape)
+    
+        # loop over the (x, y)-coordinates for the facial landmarks
+        # and draw them on the image
+        for (x, y) in shape:
+            cv2.circle(frameDlibHog, (int(x*scaleWidth), int(y*scaleHeight)), 1, (0, 0, 255), -1)
+
+    return frameDlibHog, bboxes
---
+++ pytorch.py
@@ -0,0 +1,93 @@
+import torch
+import torch.nn as nn
+from torch import optim
+import csv
+import time
+import random
+import numpy as np
+
+class Model(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.hidden = nn.Linear(68, 50)
+        self.output = nn.Linear(50, 6)
+    
+    def forward(self, x):
+        x = self.hidden(x)
+        x = torch.relu(x)
+        x = self.output(x)
+        x = torch.softmax(x, dim=0)
+        return x
+
+def getIndexMax(tensor):
+    t_max = tensor.max()
+    for i in range(len(tensor)):
+        if tensor[i] == t_max:
+            return i
+
+def getIndexMin(tensor):
+    t_min = tensor.min()
+    for i in range(len(tensor)):
+        if tensor[i] == t_min:
+            return i
+
+emotions = ["angry", "fear", "happy", "sad", "surprise", "neutral"]
+
+# model = torch.load("model.pt")
+model = Model()
+
+lossFunc = nn.BCELoss()
+optimizer = optim.SGD(model.parameters(), lr=0.1, weight_decay= 1e-6, momentum = 0.9, nesterov = True)
+
+trainingData = []
+validData = []
+trainloader = []
+validloader = []
+with open("trainingdata.csv", "r") as csv_read:
+    csvRead = csv.DictReader(csv_read)
+    for row in csvRead:
+        trainingData.append([int(row["emotion"]), list(map(lambda x: x / 2304, list(map(int, row["predictionPoints"].split()))))])
+    for data in trainingData:
+        x = torch.Tensor(data[1])
+        y = torch.Tensor([[0.0], [0.0], [0.0], [0.0], [0.0], [0.0]])
+        y[data[0]] = 1.0
+        trainloader.append([x, y])
+with open("testingdata.csv", "r") as csv_read:
+    csvRead = csv.DictReader(csv_read)
+    for row in csvRead:
+        validData.append([int(row["emotion"]), list(map(lambda x: x / 2304, list(map(int, row["predictionPoints"].split()))))])
+    for data in trainingData:
+        x = torch.Tensor(data[1])
+        y = torch.Tensor([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+        y[data[0]] = 1.0
+        validloader.append([x, y])
+
+
+
+for epoch in range(1, 10000): ## run the model for 100 epochs
+    train_loss, valid_loss = [], []
+    if epoch % 10 == 0:
+        torch.save(model, "model.pt")
+
+    model.train()
+    for data in trainloader:
+        optimizer.zero_grad()
+        output = model(data[0])
+        loss = lossFunc(torch.softmax(output, dim=0), data[1])
+        loss.backward()
+        optimizer.step()
+        train_loss.append(loss.item())
+    correct = 0
+    model.eval()
+    for data in validloader:
+        
+        output = model(data[0])
+        if emotions[getIndexMax(output)] == emotions[getIndexMax(data[1])]:
+            correct += 1
+
+        loss = lossFunc(torch.sigmoid(output), data[1])
+        valid_loss.append(loss.item())
+    if (epoch % 20 == 0):
+        torch.save(model, "model.pt")
+    print ("Epoch:", epoch, "Training Loss: ", np.mean(train_loss), "Valid Loss: ", np.mean(valid_loss), "Accuracy: ", correct / 1077 * 100)
+
